081cf1ef252a1fb709623c32098bc2a284c9bbd9
Steven Li <steven.li@walmartlabs.com>
2018-07-29 22:32:57 -0700

    working post reading

--- main.go
+++ main.go
@@ -75,6 +75,47 @@ func main() {
 		}
 	})
 
+	http.HandleFunc("/testbalances" , func(w http.ResponseWriter, r *http.Request) {
+
+		// @section1: reading the response body
+		type Balance struct{
+			Address string
+			Contract string
+		}
+
+		var b Balance
+
+		if r.Body == nil {
+			http.Error(w, "Please send a request body", 400)
+			return
+		}
+
+		err := json.NewDecoder(r.Body).Decode(&b)
+
+		if err != nil {
+			http.Error(w, err.Error(), 400)
+			return
+		}
+		
+		// @section2: creating a post request to our heroku api
+		var jsonStr = []byte(`{"Address":"` + b.Address + `",` + `"Contract":"` + b.Contract + `"}`)
+		req, error := http.NewRequest("POST", "https://web3-challenge-heroku.herokuapp.com/balances", bytes.NewBuffer(jsonStr))
+    req.Header.Set("X-Custom-Header", "myvalue")
+    req.Header.Set("Content-Type", "application/json")
+
+    client := &http.Client{}
+    resp, error := client.Do(req)
+    if error != nil {
+        panic(error)
+    }
+		defer resp.Body.Close()
+		
+		fmt.Println("response Status:", resp.Status)
+    fmt.Println("response Headers:", resp.Header)
+    body, _ := ioutil.ReadAll(resp.Body)
+		fmt.Println("response Body:", string(body))
+	})
+
 	fmt.Println("Listening");
 	fmt.Println(http.ListenAndServe(":8080", nil));
 }
